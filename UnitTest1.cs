b7203441957a3e25b066873e25c1aabe5b3a5678
Neil Sorensen <nsorens@gmail.com>
2024-05-01 19:08:56 -0600

    Set up code

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace roman_numeral_kata;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
